8a59ae6183eb643b15b7a60bb398017d74c2c692
SamTan <samsite_tan@hotmail.com>
2019-11-30 15:08:26 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,19 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne;
+cardOne = cards[3];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+let cardTwo;
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match!");
+	} else {
+		alert("Sorry, Try again.");
+	}
+}
