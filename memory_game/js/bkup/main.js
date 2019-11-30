1325a1731847ff367891b666e269ab173f53967a
SamTan <samsite_tan@hotmail.com>
2019-11-30 17:25:15 +0800

    created game logic

---
+++ memory_game/js/bkup/main.js
@@ -0,0 +1,20 @@
+console.log("Up and running!");
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne;
+let cardTwo;
+
+cardOne = cards[3];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match!");
+	} else {
+		alert("Sorry, Try again.");
+	}
+}
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,19 @@
 console.log("Up and running!");
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne;
-cardOne = cards[3];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-let cardTwo;
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
 
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You've found a match!");
+		console.log("You've found a match!");
 	} else {
-		alert("Sorry, Try again.");
+		console.log("Sorry, Try again.")
+	}
+}
+
+function flipCard(cardId) {
+	console.log("User flipped a " + cards[cardId] + ".")
+	cardsInPlay.push(cards[cardId])
+	if (cardsInPlay.length === 2) {
+		checkForMatch()
 	}
 }
